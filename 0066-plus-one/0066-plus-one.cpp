9a6d688f2842d4f2c465504f7098263e809fff9d
Shubham Hadkar <shubhamhadkar21@gmail.com>
2026-04-06 20:42:02 +0530

    Time: 0 ms (100%), Space: 11.6 MB (27.05%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int len = digits.size();
+
+        for (int i = len - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
